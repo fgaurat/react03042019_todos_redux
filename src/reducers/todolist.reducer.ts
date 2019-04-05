bf804ff43e9d4af782e3312498627964eb53bf17
Frédéric Gaurat <fgaurat@eolem.com>
2019-04-05 16:38:01 +0200

    commit

--- src/reducers/todolist.reducer.ts
+++ src/reducers/todolist.reducer.ts
@@ -3,8 +3,12 @@
 
 const reducer = (state:any={todos:[]},action:any) =>{
     console.log("todolist",action);
+    switch(action.type){
+        case 'LOAD_TODOS':
+            return {todos:action.todos};
+        default:
+            return state;
+    }
 
-    return state;
 }
-
 export default reducer;
